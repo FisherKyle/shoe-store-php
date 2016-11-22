d5e00d987d7b84c017d2dcd467c9d81df8f183e9
kyle <kyle@kylefisher.com>
2016-11-22 07:11:29 -0500

    create PHP class ojects brand and store, the database and its tables, the remaining initial file structure.

---
+++ app/app.php
@@ -0,0 +1,21 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+     date_default_timezone_set('America/New_York');
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use($app){
+        return $app['twig']->render('index.html.twig');
+    });
---
+++ src/Brand.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Brand {
+        private $name;
+        private $id;
+
+        function __construct($name, $id=null) {
+            $this->name = $name;
+            $this->id = $id;
+        }
+        
+    }
+ ?>
