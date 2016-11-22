181276013f29c503445f32e6130447b21b23d3ff
kyle <kyle@kylefisher.com>
2016-11-22 07:26:18 -0500

    begin establishing necessary functions within the class objects

--- src/Store.php
+++ src/Store.php
@@ -8,6 +8,34 @@
             $this->name = $name;
             $this->id = $id;
         }
-        
+
+        function getName() {
+            return $this->name;
+        }
+
+        function getId() {
+            return $this->id;
+        }
+
+        function getAll() {
+
+            }
+           return $stores;
+        }
+
+        function save() {
+
+        }
+
+        function delete() {
+
+        }
+
+        function saveBrand() {
+
+        }
+
+        function get
+
     }
  ?>
